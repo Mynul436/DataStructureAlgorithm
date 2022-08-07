254fc8a4f92f8bc200f742ee9a0af3d36c256d4d
Mynul Islam <mynulakash436@gmail.com>
2022-08-07 16:14:52 +0600

    Head Tail Recursion

---
+++ DataStructure/Recurtion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurtion
+{
+    public class Program
+    {
+        public void Calculate(int n)
+        {
+            if (n > 0)
+            {
+                // Head recursion 
+                Calculate(n - 1);
+                int k = n * n;
+                Console.WriteLine(k);
+               // Calculate(n-1);//Tail recursion
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Calculate(10);
+            Console.ReadKey();
+            
+        }
+    }
+}
